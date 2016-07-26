0e53a0ef7023a5a2ba77f838c1d660d15bdddb5d
BeneveCoding <514094893@qq.com>
2016-07-26 16:32:32 +0800

    搭建项目环境：测试负载均衡的规则
    负载均衡总的来说是个坑爹的东西，但是可以接受

--- jeve/jv-service-example/src/main/java/org/beneve/service/example/controller/Demo1Controller.java
+++ jeve/jv-service-example/src/main/java/org/beneve/service/example/controller/Demo1Controller.java
@@ -7,6 +7,8 @@
  */
 package org.beneve.service.example.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -29,10 +31,28 @@ public class Demo1Controller {
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
-        ServiceInstance instance = client.getLocalServiceInstance();
+
+        List<String> servicesList = client.getServices();
+        for (String serviceId : servicesList) {
+            List<ServiceInstance> instanceList = client.getInstances(serviceId);
+            for (ServiceInstance instance : instanceList) {
+            System.out.println("#######################################");
+            System.out.println("ServiceId: " + instance.getServiceId());
+            System.out.println("Host: " + instance.getHost());
+            System.out.println("Port: " + instance.getPort());
+            System.out.println("Uri: " + instance.getUri());
+            System.out.println("Metadata: " + instance.getMetadata().toString());
+            System.out.println("isHttps: " + instance.isSecure());
+            }
+        }
+
+        ServiceInstance ThisInstance = client.getLocalServiceInstance();
+
         Integer r = a + b;
-        System.out.println("/add, host:" + instance.getHost() + ", service_id:"
-                + instance.getServiceId() + ", result:" + r);
+        System.out.println("/add, host:" + ThisInstance.getHost() + ", service_id:"
+                + ThisInstance.getServiceId() + ", result:" + r);
+
+        System.out.println("_____________________________________");
         return r;
     }
 }
